f9b4156d9df51b9015bf55edaf272630b56a8799
Michael Lu <mike.lu.92@gmail.com>
2018-02-18 13:22:35 -0800

    changed numpad, added raise to left side for one handed F keys

--- keyboards/lets_split/keymaps/mikelu/keymap.c
+++ keyboards/lets_split/keymaps/mikelu/keymap.c
@@ -28,7 +28,7 @@ enum custom_keycodes {
 #define SFT_ENT  SFT_T(KC_ENT) // Enter when tapped, shift when held
 #define CTL_ESC  CTL_T(KC_ESC) // Esc when tapped, ctrl when held
 #define GUI_CTL  MT(MOD_LGUI, OSM(MOD_LCTL)) // Ctrl when tapped, gui (command) when held
-#define TAP_LWR  OSL(_LOWER) // tap for lower
+#define TAP_RSE  OSL(_RAISE) // tap to raise
 
 
 
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Esc  |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Lower |Adjust| Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
+ * |Raise |Adjust| Alt  | GUI  |Lower |Space |Enter |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  *
  *  Hold
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   KC_BSPC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
   CTL_ESC, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_EQL , \
-  TAP_LWR, ADJUST,  KC_LALT, KC_LGUI,  LOWER,   KC_SPC,  SFT_ENT, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  TAP_RSE, ADJUST,  KC_LALT, KC_LGUI, LOWER,   KC_SPC,  SFT_ENT, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Qwerty
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Bksp |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  -   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  Del |   :  |   =  |   {  |   }  |      |      |      |   [  |   ]  |      |  =   |
+ * |  Del |   :  |   =  |   {  |   }  |      |      |   [  |   ]  |      |      |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -102,20 +102,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |  F9  |  F10 |  F11 | F12  | Home | PgUp |   7  |   8  |   9  |   /  |  ?   |
+ * |   ~  |  F9  |  F10 |  F11 | F12  | HOME | PGUP |      |   7  |   8  |   9  |   /  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F5  |  F6  |  F7  |  F8  | End  | PgDn |   4  |   5  |   6  |   *  |  _   |
+ * | DEL  |  F5  |  F6  |  F7  |  F8  | END  | PGDN |      |   4  |   5  |   6  |   *  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F1  |  F2  |  F3  |  F4  |      |      |   1  |   2  |   3  |   -  |  +   |
+ * |      |  F1  |  F2  |  F3  |  F4  |      |      |      |   1  |   2  |   3  |   -  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |   0  |   .  | Entr |      |
+ * |      |      |      |      |      |             |      |   .  |   0  | Entr |   +  |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = KEYMAP( \
-  KC_TILD, KC_F9,   KC_F10,  KC_F11,  KC_F12 , KC_HOME, KC_PGUP, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_QUES, \
-  KC_BSPC, KC_F5,   KC_F6,   KC_F7,   KC_F8  , KC_END,  KC_PGDN, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_UNDS, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4  , _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PLUS, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_PENT, _______, \
+  KC_TILD, KC_F9,   KC_F10,  KC_F11,  KC_F12 , KC_HOME, KC_PGUP, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, \
+  KC_BSPC, KC_F5,   KC_F6,   KC_F7,   KC_F8  , KC_END,  KC_PGDN, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4  , _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_PDOT, KC_P0,   KC_PENT, KC_PLUS  \
 ),
 
 /* Adjust (Lower + Raise)
