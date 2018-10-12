04cb81683b8b3e506a41db462317b174dcc4950d
Michael Lu <mike.lu.92@gmail.com>
2018-10-12 11:09:33 -0500

    Add ergotravel

---
+++ keyboards/ergotravel/keymaps/mikelu92/keymap.c
@@ -0,0 +1,178 @@
+﻿#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
+
+#define _DVORAK 0
+#define _QWERTY 1
+#define _LOWER 2
+#define _RAISE 3
+#define _ADJUST 16
+
+enum custom_keycodes {
+  DVORAK = SAFE_RANGE,
+  QWERTY,
+  LOWER,
+  RAISE,
+  ADJUST,
+  TABU,
+  TABD,
+  VIMTABU,
+  VIMTABD,
+};
+
+// #define KC_ KC_TRNS
+#define _______ KC_TRNS
+#define SFT_ENT  SFT_T(KC_ENT) // Enter when tapped, shift when held
+#define CTL_ESC  CTL_T(KC_ESC) // Esc when tapped, ctrl when held
+#define GUI_CTL  MT(MOD_LGUI, OSM(MOD_LCTL)) // Ctrl when tapped, gui (command) when held
+#define TAP_RSE  OSL(_RAISE) // tap to raise
+#define TAP_SFT  OSM(MOD_LSFT) // tap shift
+#define TAP_ALT  OSM(MOD_LALT) // tap alt
+#define TAP_HPR  OSM(MOD_HYPR) // tap hyper
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_DVORAK] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_HOME,          KC_PGUP, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_BSPC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_END,           KC_PGDN, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     CTL_ESC, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    TAP_ALT,          KC_MEH,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_EQL,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     TAP_RSE, TAP_SFT, TAP_HPR, KC_LGUI,          LOWER,   KC_SPC,           SFT_ENT, RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  ),
+
+  [_QWERTY] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,          KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_A,             KC_A,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_A,             KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LCTL, KC_LGUI, KC_LALT, ADJUST,           LOWER,   KC_SPC,           KC_SPC,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  ),
+
+  [_LOWER] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TABU,             VIMTABU, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, TABD,             VIMTABD, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_DEL,  KC_COLN, KC_EQL,  KC_LCBR, KC_RCBR, _______, _______,          _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_EQL,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, _______, _______, _______,          _______, _______,          _______, _______,          KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  ),
+
+  [_RAISE] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     _______,  KC_F9,   KC_F10,  KC_F11,  KC_F12, _______, TABU,             VIMTABU, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, TABD,             VIMTABD, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______,          _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, _______, _______, _______,          _______, _______,          _______, _______,          KC_PDOT, KC_P0,   KC_PENT, KC_PLUS 
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  ),
+
+  [_ADJUST] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     _______, RESET,   _______, KC_UP,   _______, _______, _______,          _______, _______, _______, KC_UP,   _______, _______, _______,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,          _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, QWERTY,  DVORAK,  _______,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, _______, _______, _______,          _______, _______,          _______, _______,          _______, _______, _______, _______
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  )
+
+};
+
+
+
+
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_qwerty);
+        #endif
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case DVORAK:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_dvorak);
+        #endif
+        set_single_persistent_default_layer(_DVORAK);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+    case VIMTABD:
+      if (record->event.pressed) {
+              SEND_STRING("gt");
+      }
+      return false;
+      break;
+    case VIMTABU:
+      if (record->event.pressed) {
+              SEND_STRING("gT");
+      }
+      return false;
+      break;
+    case TABD:
+      if (record->event.pressed) {
+             SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_TAB)SS_UP(X_LCTRL));
+      }
+      return false;
+      break;
+    case TABU:
+      if (record->event.pressed) {
+             SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LSHIFT)SS_TAP(X_TAB)SS_UP(X_LCTRL)SS_UP(X_LSHIFT));
+      }
+      return false;
+      break;
+  }
+  return true;
+}
