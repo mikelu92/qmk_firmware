e152378c155f55f783a64df447cd67fef246e2ee
Michael Lu <mike.lu.92@gmail.com>
2019-06-05 15:19:26 -0500

    Update ergotravel

--- keyboards/ergotravel/keymaps/mikelu92/keymap.c
+++ keyboards/ergotravel/keymaps/mikelu92/keymap.c
@@ -53,7 +53,7 @@ enum custom_keycodes {
 #define SWPU LCTL(KC_UP)
 #define SWPR LCTL(KC_RIGHT)
 #define CHKESC HYPR(KC_C)
-#define TMUXPRF LCTL(LALT(KC_A))
+#define TMUXPRF LCTL(KC_A)
 #define VIMWIND LCTL(KC_W)
 #define ALT_L LALT(KC_LEFT)
 #define ALT_R LALT(KC_RGHT)
@@ -61,6 +61,8 @@ enum custom_keycodes {
 #define ALT_D LALT(KC_DOWN)
 #define WIN_L LGUI(LSFT(KC_TILD))
 #define WIN_R LGUI(KC_TILD)
+#define CTL_U LCTL(KC_U)
+#define CTL_D LCTL(KC_D)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -95,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, TABD,             NOTE_DN, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_DEL,  KC_COLN, KC_TILD, KC_LCBR, KC_RCBR, KC_TILD, _______,          _______, KC_PIPE, KC_LBRC, KC_RBRC, TABU,    TABD,    COL_EQL,
+     KC_DEL,  KC_COLN, KC_TILD, KC_LCBR, KC_RCBR, _______, _______,          _______, KC_PIPE, KC_LBRC, KC_RBRC, TABU,    TABD,    COL_EQL,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      DSP_OFF, WIN_L,   WIN_R,   _______,          _______, _______,          _______, _______,          ALT_CMD, KC_VOLD, KC_VOLU, KC_MUTE
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
@@ -103,11 +105,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
-     KC_GRV,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  FOCUSMD, TABU,             VIMTABU, SSH_SEU, SSH_C7,  CHKESC,  _______, _______, KEYPAD,
+     KC_GRV,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  FOCUSMD, TABU,             VIMTABU, _______, _______, CHKESC,  _______, _______, KEYPAD,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, TABD,             VIMTABD, _______, AUTOFIL, TMUXPRF, TABU,    TABD,    _______,
+     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   CTL_U,   TABD,             VIMTABD, _______, AUTOFIL, TMUXPRF, TABU,    TABD,    _______,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______,          _______, _______, SRCHPWD, VIMWIND, SWPL,    SWPR,    KC_BTN1,
+     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   CTL_D,   _______,          _______, _______, SRCHPWD, VIMWIND, SWPL,    SWPR,    KC_BTN1,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      _______, _______, _______, _______,          _______, _______,          _______, _______,          LSCRN,   DSCRN,   USCRN,   RSCRN 
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
@@ -125,17 +127,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_KEYPAD] = LAYOUT(
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
-     _______, _______, _______, _______, _______, _______, TABU,             VIMTABU, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_PSLS,
+     _______, _______, KC_P7,   KC_P8,   KC_P9,   _______, TABU,             VIMTABU, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_PSLS,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_BSPC, _______, _______, _______, _______, _______, TABD,             VIMTABD, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_PMNS,
+     KC_BSPC, _______, KC_P4,   KC_P5,   KC_P6,   _______, TABD,             VIMTABD, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_PMNS,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     _______, _______, _______, _______, _______, _______, DVORAK,           DVORAK,  _______, KC_P1,   KC_P2,   KC_P3,   KC_UP,   KC_PPLS,
+     _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, DVORAK,           DVORAK,  _______, KC_P1,   KC_P2,   KC_P3,   KC_UP,   KC_PPLS,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     _______, _______, _______, _______,          _______, _______,          KC_PENT, KC_PDOT,          KC_P0,   KC_LEFT, KC_DOWN, KC_RGHT 
+     _______, _______, KC_PDOT, KC_P0,            _______, _______,          KC_PENT, KC_PDOT,          KC_P0,   KC_LEFT, KC_DOWN, KC_RGHT 
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
   )
-
-
 };
 
 
